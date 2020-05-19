f940ba1b69a7c823eaafc338af873f56668c3349
yhjflower <1131335234@qq.com>
2020-05-19 15:45:33 +0800

    第一周代码作业

---
+++ week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -0,0 +1,15 @@
+# python3
+
+
+def max_pairwise_product(numbers):
+    a=max(numbers)
+	numbers.remove(a)
+	b=max(numbers)
+
+    return a*b
+
+
+if __name__ == '__main__':
+    input_n = int(input())
+    input_numbers = [int(x) for x in input().split()]
+    print(max_pairwise_product(input_numbers))
